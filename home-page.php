6c8542e8b77eb1fb03991933236906fccd597043
Gabriel Marinho <gsmarinho@id.uff.br>
2022-08-13 17:35:21 -0300

    feat:inicio hardcode home

---
+++ home-page.php
@@ -0,0 +1,21 @@
+<?php
+// Template Name: home
+?>
+<!DOCTYPE html>
+<html>
+<head lang="pt-br">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<title>Comes & Bebes - Home</title>
+</head>
+<body>
+    <main>
+        <div id="content-home">
+            <div id="titulo-content-home">
+                <h2>CONHEÇA NOSSA LOJA</h2>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
